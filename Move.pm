5c8d1efc21d061ff10b57fc3a23b9dcece1887f6
Dee Newcum <dee.newcum@gmail.com>
2013-10-31 21:46:12 -0500

    the start of Move->apply

--- Move.pm
+++ Move.pm
@@ -22,6 +22,14 @@ my %dir_chars = qw(
         4  <
 );
 my %dir_chars_inverse = reverse %dir_chars;
+my @direction = (
+    [0,0],
+    [0,1],      # 1 = up
+    [1,0],      # 2 = right
+    [0,-1],     # 3 = down
+    [-1,0],     # 4 = left
+);
+
 
 around BUILDARGS => sub {
     my $orig = shift;
@@ -46,5 +54,43 @@ sub toString {
             $self->y + 1,
             $dir_chars{$self->dir};
 }
+
+# Move the pieces on the board to reflect the specified move.
+#
+# Returns true if it was a legal move, false if it was illegal.
+sub apply {
+    my $self = shift;
+    my ($board) = @_;
+
+    return 0 if (!_in_bounds($board, $self->x, $self->y));
+
+    my $cell = $board->{cells}[$self->y][$self->x];
+    my @dir = @{ $direction[ $self->dir ] };
+    
+    if (abs($cell) > 10 && $cell % 100 != 0) {
+        # This isn't a movable cell.  This isn't a legal move.
+        return 0;
+    }
+
+    my @just_before_collision = ($self->x, $self->y);
+    my @just_after_collision  = ($self->x, $self->y);
+
+    do {
+        @just_before_collision = @just_after_collision;
+        $just_after_collision[0] += $dir[0];
+        $just_after_collision[1] += $dir[1];
+    } while (_in_bounds($board, @just_after_collision)
+            && $board->{cells}[$just_after_collision[0]][$just_after_collision[1]] == -11);
+
+    # LEFTOFF -- continue from here...  figure out what to do now that we hit something
+}
+
+    # returns true/false, indicating if the point lies inside the board
+    sub _in_bounds {
+        my ($board, $x, $y) = @_;
+        return ($x >=0 && $x < $board->width
+                && $y >=0 && $y < $board->height);
+    }
+
 
 1;
