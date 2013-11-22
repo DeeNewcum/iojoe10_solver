affb2a27ab2fbffb3e89d2acf6b89144441ba9a3
Dee Newcum <dee.newcum@gmail.com>
2013-11-22 15:48:02 -0600

    m

--- boards/pcs.sh
+++ boards/pcs.sh
@@ -2,4 +2,4 @@
 
 # Display the size of all boards, sorted by number of pieces.
 
-grep -I [p]cs * 2>/dev/null | sort -n -k 3 | perl -F':#' -nale 'printf "%15s %s\n", @F'
+grep -I [p]cs * 2>/dev/null | sort -n -k 3 | perl -F':#' -nale 'printf "%-15s %s\n", @F'
