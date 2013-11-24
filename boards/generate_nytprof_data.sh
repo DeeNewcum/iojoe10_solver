0be81fda8c8d543eee2872d9df150eeba8ec6342
Dee Newcum <dee.newcum@gmail.com>
2013-11-24 11:48:23 -0600

    store nytprof.out file in ramdisk

--- boards/generate_nytprof_data.sh
+++ boards/generate_nytprof_data.sh
@@ -8,17 +8,37 @@ rm -rf nytprof.old*
 rm -rf nytprof.new*
 
 
+
+# Put the nytprof.out file in a ramdisk -- it's ~1gb, and all that activity can reduce the lifetime of my SSD.
+OUTFILE_LOCATION=/tmp/nytprof.out       # Where will the nytprof.out file be located?  (it can be quite large)
+# There's a ramdisk already at /run/user/$USER/, however it's awfully small.  I've got 8gb RAM, let's use it!
+mount | grep /media/nytprof.ramdisk >/dev/null
+if [ $? != 0 ]; then
+    [ -d /media/nytprof.ramdisk/ ] || sudo mkdir -p /media/nytprof.ramdisk/
+    sudo mount -t tmpfs -o size=2048M tmpfs /media/nytprof.ramdisk/
+fi
+mount | grep /media/nytprof.ramdisk >/dev/null
+if [ $? = 0 ]; then
+    OUTFILE_LOCATION=/media/nytprof.ramdisk/nytprof.out
+else
+    echo UNABLE TO STORE nytprof.out ON SSD
+fi
+export NYTPROF="file=$OUTFILE_LOCATION"
+
+
+
 ######## "new" configuration ########
 perl -d:NYTProf ../iojoe10 $BOARD
-mv nytprof.out nytprof.new.out
-nytprofhtml --file nytprof.new.out --out nytprof.new
+nytprofhtml --file $OUTFILE_LOCATION --out nytprof.new
+rm $OUTFILE_LOCATION          # this thing is ~1GB!
 
 ######## "old" configuration ########
             # --compare-old is a special flag, that I use specifically when doing A/B testing,
             #                   to specify the alternate configuration I want to test with
 perl -d:NYTProf ../iojoe10 $BOARD --compare-old
-mv nytprof.out nytprof.old.out
-nytprofhtml --file nytprof.old.out --out nytprof.old
+nytprofhtml --file $OUTFILE_LOCATION --out nytprof.old
+rm $OUTFILE_LOCATION          # this thing is ~1GB!
+
 
 xdg-open nytprof.new/index.html
 xdg-open nytprof.old/index.html
