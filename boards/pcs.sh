7f0e42f7fe3921c053783ab0cf0befdd988dfa86
Dee Newcum <dee.newcum@gmail.com>
2013-11-23 20:34:39 -0600

    document how to list missing 'pcs' fields

--- boards/pcs.sh
+++ boards/pcs.sh
@@ -3,3 +3,7 @@
 # Display the size of all boards, sorted by number of pieces.
 
 grep -I [p]cs * 2>/dev/null | sort -n -k 3 | perl -F':#' -nale 'printf "%-15s %s\n", @F'
+
+
+# to list boards that don't haven't properly listed their number of pieces, run this:
+#       \ls -1 | perl -nle 'next if (-d || !-x || /\.sh$/); print unless qx[grep pcs $_]'
